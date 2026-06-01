ebcf1ef1addaa01d38738644e9716765b76bb4eb
RohiniM1607 <mrohini7558@gmail.com>
2026-06-01 16:48:40 +0530

    Handson - Sequential

--- Sequential/H5_PriceCalculator.py
+++ Sequential/H5_PriceCalculator.py
@@ -1,11 +1,17 @@
 total = 0
 count = 1
+
 while True:
     price = float(input(f"Enter the price of the {count} item: "))
     quantity = int(input(f"Enter the quantity of the {count} item: "))
-    total = total + (price * quantity)
+
+    total += price * quantity
+
     choice = input("Do you want to enter another item? (yes/no): ")
-    if choice.lower() == "no":
+    print(f"Choice entered = [{choice}]")
+
+    if choice.strip().lower() == "no":
         break
     count += 1
+
 print("Total Price:", int(total))
