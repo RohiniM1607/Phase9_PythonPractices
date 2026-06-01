3659e012031f549e100998b31cd143f07cfb6558
RohiniM1607 <mrohini7558@gmail.com>
2026-06-01 20:19:50 +0530

    Handson - Iteration

--- Iteration/H5_PrimeFactors.py
+++ Iteration/H5_PrimeFactors.py
@@ -1,8 +1,9 @@
 num = int(input())
+
 factor = 2
+
 while num > 1:
     while num % factor == 0:
         print(factor, end=" ")
         num = num // factor
-    factor = factor + 1
-print()
+    factor += 1
